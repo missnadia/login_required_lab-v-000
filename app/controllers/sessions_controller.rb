16aeceef583e24ae8c8fc6b23a2cfc8d2d55bf4f
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-19 22:00:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    controller: 'application', action: 'hello'
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
